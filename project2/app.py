910dcbe68774b7dd7c6aea1f8691cd99ff68c427
Your Name <nickyoungdo@naver.com>
2021-08-28 19:00:49 +0900

    update

--- project2/app.py
+++ project2/app.py
@@ -18,7 +18,10 @@ def main():
 @app.route('/detail/<keyword>')
 def detail(keyword):
     # API에서 단어 뜻 찾아서 결과 보내기
-    return render_template("detail.html", word=keyword)
+    r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}", headers={"Authorization": "Token fab3372dd6c05f5d404f0491bb9cefc79044663d"})
+    result = r.json()
+    print(result)
+    return render_template("detail.html", word=keyword,result=result)
 
 
 @app.route('/api/save_word', methods=['POST'])
